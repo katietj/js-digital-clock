454b41cf8446cde056a8b3a7c2152e9d382f5aa8
Katie Jahanmir <katietj@gmail.com>
2018-11-19 08:42:30 -0800

    completed digital clock

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+
+
+
+const getTime = function(){
+  const date = new Date();
+
+  let hours = (date.getHours());
+  let minutes = (date.getMinutes());
+  let seconds = (date.getSeconds());
+
+  let display = `${date.toLocaleDateString()} ${hours}:${minutes}:${seconds}`;
+  $('#clock').html(display);
+};
+
+
+$(document).ready(function() {
+
+  setInterval(getTime, 1000);
+});
